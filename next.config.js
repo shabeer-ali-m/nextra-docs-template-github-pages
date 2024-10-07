593fd19ab5299727c85f30f285d94bcb184e458e
Shabeer Ali M <shabeer.ali.m@gmail.com>
2024-10-07 21:25:23 +0200

    Build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true // mandatory, otherwise won't export
+  },
+  //distDir: "build"
+}
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra(nextConfig)
